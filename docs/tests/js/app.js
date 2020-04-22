67dc2850af538dd08f1ff5f02331a748dcda9e47
drdynscript <drdynscript@gmail.com>
2020-04-23 00:06:11 +0200

    Added tests

--- docs/tests/js/app.js
+++ docs/tests/js/app.js
@@ -1,1 +1,11 @@
-alert('hello');
+(() => {  
+  const DOMAIN = 'https://pgmgent-1920-students.github.io/atwork2-case1-baas';
+
+  const loadBlog = async () => {
+    const response = await fetch(`${DOMAIN}/data/blog/index.json`);
+    const jsonData = await response.json();
+    console.log(jsonData);
+  }
+
+  loadBlog();
+})();
